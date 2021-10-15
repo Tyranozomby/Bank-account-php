0b65e44f0fea260a10a7e3c2e9b7159950727a0a
Bilel Medimegh <bilel.medimegh@gmail.com>
2021-10-15 17:00:32 +0200

    Add files via upload

---
+++ processlog.php
@@ -0,0 +1,17 @@
+<?php
+
+session_start();
+if($_SESSION["admin"] != "admin"){
+    header("Location: adminlogin.php");
+}
+
+if(isset($_GET["archiver"])){
+    //archiver logs
+    rename("logs.csv","logs.csv.archive");
+}
+else if(isset($_GET["vider"])){
+    //archiver logs
+    unlink("logs.csv");
+}
+
+header("Location: admin.php");
