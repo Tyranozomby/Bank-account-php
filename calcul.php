95553bc1a561edc3ace567b9a9fc7e05c8e21b73
Tyranozomby <tyranozomby@gmail.com>
2021-10-15 20:49:12 +0200

    Ajout de sécurité sur calcul.php

--- calcul.php
+++ calcul.php
@@ -1,21 +1,49 @@
 <?php
 
-if (isset($_GET["montant"], $_GET["capital"], $_GET["nombre_mois"], $_GET["taux"])
-//    and is_int($_GET["montant"]) and is_int($_GET["capital"]) and is_int($_GET["nombre_mois"]) and is_int($_GET["taux"])
-) {
+// Vérifie l'existence des variables nécessaires au calcul sinon redirige
+if (isset($_GET["capital"], $_GET["nombre_mois"], $_GET["taux"])) {
 
-    $montant = $_GET["montant"];
-    $capital = $_GET["capital"];
-    $nombre_mois = $_GET["nombre_mois"];
-    $taux = $_GET["taux"];
+    // Crée les variables $capital, $nombre_mois, $taux en vérifiant que ce sont bien des nombres sinon redirige
+    foreach ($_GET as $k => $v) {
+        if (is_numeric($v)) {
+            $$k = (int)$v;
+        } else {
+            redirect();
+        }
+    }
 
+    // Formule de calcul du montant
     $montant = ($capital * ($taux / 12)) / (1 - (1 + ($taux / 12)) ** (-$nombre_mois));
-    echo $montant;
+    echo $montant, PHP_EOL;
 
-    $f = fopen("logs.cvs", 'a');
-    $array = array($_SERVER['REMOTE_ADDR'], getdate(), $montant, $capital, $nombre_mois, $taux);
+    // Crée le fichier et ajoute les colonnes s'il n'existe pas
+    if (!file_exists("logs.csv")) {
+        $f = fopen("logs.csv", 'w');
+        $array = array("ip", "date", "Montant", "Capital", "Nombredemois", "Taux");
+        fputcsv($f, $array);
+        fclose($f);
+    }
+
+    // Essaye d'ouvrir le fichier et boucle tant qu'il n'y arrive pas (c'est qu'il est utilisé par un autre utilisateur)
+    $f = fopen("logs.csv", 'a');
+    if ($f == false) {
+        while ($f == false) {
+            usleep(10);
+            $f = fopen("logs.csv", 'a');
+        }
+    }
+
+    // Rempli les logs avec les valeurs nécessaires
+    $array = array($_SERVER['REMOTE_ADDR'], time(), $montant, $capital, $nombre_mois, $taux);
     fputcsv($f, $array);
+    fclose($f);
 
 } else {
+    redirect();
+}
+
+function redirect()
+{
     header("Location: simulation.php");
+    exit();
 }
