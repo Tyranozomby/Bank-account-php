c895168ea89c86cf2a6bc9a92ae8351ffe9b55f0
B-bigdata <76886674+B-bigdata@users.noreply.github.com>
2021-10-17 21:00:00 +0200

    Add files via upload

--- adminlogin.php
+++ adminlogin.php
@@ -1,1 +1,43 @@
-<?php
+<!doctype html>
+<html>
+
+
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="styleAdminlogin.css" />
+	<div align="center">
+    <title>Connexion</title>
+
+    <body>
+		<div class="container">
+		<?php
+			echo"<p>Bienvenue</p>";
+			//lien de retour
+			if(isset($_GET['id'])){
+				switch ($_GET['id']){
+					case "1":
+						echo "<p class='machin' style='color: red'>Erreur identifiant</p>";
+						break;
+					case "2":
+						echo "<p class='machin' style='color: blue'>Connectez-vous</p>";
+						break;
+					case "3":
+						echo "<p class='machin' style='color: green'>Deconnecté</p>";
+						break;
+					case "4":
+						echo "<p class='machin' style='color: black'>Impossible de se reconnecter sur cette page </p>";
+						break;
+				}
+			}
+			//formulaire
+			echo "<form action='dologin.php' method='post'><br>
+			Login&nbsp&nbsp&nbsp<input type='text' name='login'><br><br><br>
+			Mdp&nbsp&nbsp&nbsp<input type='password' name='password'></p>
+			<input type='submit' name='ok' value='Connexion'>
+			<br><br>
+			";
+
+		?>
+		
+</html>
+
+
--- simulation.php
+++ simulation.php
@@ -1,1 +1,36 @@
-<?php
+<!doctype html>
+<html>
+
+
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="styleSimulation.css" />
+	<div align="center">
+    <title>Connexion</title>
+
+    <body>
+		<div class="container">
+		<?php
+			echo"<p>Pret Bancaire</p>";
+			//lien de retour
+			if(isset($_GET['id'])){
+				switch ($_GET['id']){
+					case "1":
+						echo "<p class='machin' style='color: red'>Erreur dans les champs</p>";
+						break;
+				}
+			}
+			//formulaire
+			echo "<form action='calcul.php' method='get'>
+			Montant&nbsp&nbsp&nbsp<input type='text' name='montant'><br><br><br>
+			Capital&nbsp&nbsp&nbsp<input type='text' name='capital'></p>
+			Mois&nbsp&nbsp&nbsp<input type='text' name='nombre_mois'></p>
+			Taux&nbsp&nbsp&nbsp<input type='text' name='taux'></p>
+			<input type='submit' name='calculer' value='Calculer'>
+			<br><br>
+			";
+		
+		?>
+		
+</html>
+
+
