bb3094fd3d3fe952c6fa80592d39d66549383fb9
Raphaelgruet <92589374+Raphaelgruet@users.noreply.github.com>
2021-10-17 15:15:30 +0200

    Update dologin.php

--- dologin.php
+++ dologin.php
@@ -6,16 +6,23 @@ if(isset($_POST['login'], $_POST['password']) && $_POST['login'] != "" && $_POST
         $file = "admin_pass.csv";
         $fp=fopen($file,"r");
 
-        while($data= fgetcsv($fp, 1024, ";")){
-
-            foreach($data as $v) {
-                echo $v . " | ";
-            }
+        while($data = fgetcsv($fp, 1024, ";")){
+			
+			if($data[0] == $_POST['login']){
+				
+				if(hash('sha256', $_POST['password']) == $data[1]){
+					echo "good password";
+				} else {
+					echo "wrong password";
+				}
+			}
         }
         fclose($fp);
     } else {
         echo("Le fichier <strong>admin_pass.csv</strong> n'existe pas !");
     }
+} else {
+	echo "veuillez compléter tous les champs";
 }
 
 ?>
