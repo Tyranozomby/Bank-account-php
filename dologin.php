73834c384048123afc9f4e9f9640b81459711f78
Raphaelgruet <92589374+Raphaelgruet@users.noreply.github.com>
2021-10-19 11:51:20 +0200

    Update dologin.php

--- dologin.php
+++ dologin.php
@@ -8,9 +8,9 @@ if (isset($_POST['login'], $_POST['password']) && $_POST['login'] != "" && $_POS
 
         while ($data = fgetcsv($fp, 1024, ";")) {
 
-            if ($data[0] == $_POST['login']) {
+            if ($data[0] == strip_tags($_POST['login'])) {
 
-                if (hash('sha256', $_POST['password']) == $data[1]) {
+                if (hash('sha256', strip_tags($_POST['password']) == $data[1]) {
                     header('Location: admin.php');
                     fclose($fp);
                     exit();
