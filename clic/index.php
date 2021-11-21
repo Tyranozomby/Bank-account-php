5ad68d7f16b4f9df76ceb3e4d8de458b8ad83a0d
B-bigdata <binyam.elbaz@gmail.com>
2021-11-21 15:47:54 +0100

    clic droit V.2

--- clic/index.php
+++ clic/index.php
@@ -8,19 +8,20 @@
 
 <body>
 
-<div class="box">
-    <h1 class="titre">© Projet PHP 2021</h1>
-    <p class="text">Bilel Medimegh</p>
-    <p class="text">Eliott Rogeaux</p>
-    <p class="text">Benjamin Elbaz</p>
-    <p class="text">Stéphane Lay</p>
-    <p class="text">Raphael Gruet</p>
-
-    <a class="boutonAccueil" href="#" onClick="window.close()" title="fermer la fenetre">
-        <img src="../icones/accueil.png" width="48" alt=""/>
-    </a>
+<div class="box" data-tilt data-tilt-transition="false">
+    <div style="background: rgb(31,31,3)!important; height: 100%; width: 100%; border-radius: 1.8%">
+        <h1 class="titre">© Projet PHP 2021</h1>
+        <p class="text" style="background:linear-gradient(to bottom,#e318bc 49%, #1100ff)"> Bilel Medimegh </p>
+        <p class="text" style="background:linear-gradient(to bottom,#ff0001 30%, #7500f3)">Eliott Rogeaux</p>
+        <p class="text" style="background:linear-gradient(to bottom,#c2eec2 60%,#b9d216)">Benjamin Elbaz</p>
+        <p class="text" style="background:linear-gradient(to bottom,#e318bc 49%, #f1bc0e);"> Stéphane Lay</p>
+        <p class="text" style="background:linear-gradient(to bottom,#f69d3c 50%, #3ea5e0)">Raphael Gruet</p>
+        <a class="boutonAccueil" href="#" onclick="window.close();location.href ='../index.html'" title="fermer la fenetre">
+            <img src="/icones/accueil.png" width="35" alt=""/>
+        </a>
+    </div>
 </div>
 
-
+<script src="https://cdnjs.cloudflare.com/ajax/libs/vanilla-tilt/1.7.2/vanilla-tilt.min.js"></script>
 </body>
 </html>
