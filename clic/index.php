118cf085de5a12205474551332343557b869d2a2
B-bigdata <binyam.elbaz@gmail.com>
2021-11-21 13:32:28 +0100

    clic droit

---
+++ clic/index.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<meta charset="utf-8"/>
+<html lang="fr">
+<head>
+    <title> </title>
+    <link href="styleclic.css" rel="stylesheet">
+</head>
+
+<body>
+
+<div class="box">
+    <h1 class="titre">© Projet PHP 2021</h1>
+    <p class="text">Bilel Medimegh</p>
+    <p class="text">Eliott Rogeaux</p>
+    <p class="text">Benjamin Elbaz</p>
+    <p class="text">Stéphane Lay</p>
+    <p class="text">Raphael Gruet</p>
+
+    <a class="boutonAccueil" href="#" onClick="window.close()" title="fermer la fenetre">
+        <img src="../icones/accueil.png" width="48" alt=""/>
+    </a>
+</div>
+
+
+</body>
+</html>
